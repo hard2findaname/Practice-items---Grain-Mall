ae3de81b90d7373aca6965b4c44c129464b093c5
Sophisticate1 <2982493959@qq.com>
2024-05-11 17:54:19 +0800

    项目初始结构

---
+++ grainmall-coupon/src/main/java/com/practice/grainmall/coupon/GrainmallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.practice.grainmall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrainmallCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrainmallCouponApplication.class, args);
+	}
+
+}
---
+++ grainmall-member/src/test/java/com/practice/grainmall/member/GrainmallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.grainmall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrainmallMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
