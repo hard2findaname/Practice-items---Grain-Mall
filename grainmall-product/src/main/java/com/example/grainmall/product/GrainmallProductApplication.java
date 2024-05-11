0c742c4f03d0b02fab5e3411e9fe72b30575a89c
Sophisticate1 <2982493959@qq.com>
2024-05-11 17:57:04 +0800

    项目初始结构

--- grainmall-product/src/main/java/com/example/grainmall/product/GrainmallProductApplication.java
+++ grainmall-product/src/main/java/com/example/grainmall/product/GrainmallProductApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GrainmallProductApplication {
 
 	public static void main(String[] args) {
+		int a = 0;
 		SpringApplication.run(GrainmallProductApplication.class, args);
 	}
 
